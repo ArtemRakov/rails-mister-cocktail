0d1f07baa6853336582a6c3d49739d581ee6c16c
Artem Rakov <scoff1k@gmail.com>
2017-08-17 17:40:44 +0100

    back end complete

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
+ruby '2.3.4'
+
 source 'https://rubygems.org'
 
 git_source(:github) do |repo_name|
@@ -34,6 +36,14 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+
+gem 'faker'
+
+#Front end
+
+gem 'font-awesome-sass'
+gem 'simple_form'
+gem 'autoprefixer-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+Ingredient.destroy_all
+
+response = open('http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+result = JSON.parse(response)
+result["drinks"].first(10).each do |hash|
+  Ingredient.create(name: hash["strIngredient1"])
+end
+
+
+5.times do
+  Cocktail.create(name: Faker::Coffee.blend_name)
+end
