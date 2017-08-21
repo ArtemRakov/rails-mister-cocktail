ce7478c51513e1490f3df5ea1e45be9f408e227f
Artem Rakov <scoff1k@gmail.com>
2017-08-21 11:25:36 +0100

    update the seed for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,7 @@ Cocktail.destroy_all
 
 response = open('http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
 result = JSON.parse(response)
-result["drinks"].first(10).each do |hash|
+result["drinks"].first(15).each do |hash|
   Ingredient.create(name: hash["strIngredient1"])
 end
 
-
-5.times do
-  Cocktail.create(name: Faker::Coffee.blend_name)
-end
